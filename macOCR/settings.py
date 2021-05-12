21471f40cf4e31590d611697ef6713c2af40c81a
engAbdullahHanif <engabdullahhanif@gmail.com>
2021-05-12 11:42:56 +0430

    updating settings.py file

--- macOCR/settings.py
+++ macOCR/settings.py
@@ -138,6 +138,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_file')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
